aca69ce8d32b1e1cf6f944cf235999f2312b41df
Ben <oboylel@mail.gvsu.edu>
2015-04-14 22:10:12 -0400

    Made the second argument accept an offset. Commente

--- checksumCalc.c
+++ checksumCalc.c
@@ -9,31 +9,44 @@
 unsigned int uShort(unsigned char *data, int bufSize);
 
 int main(int argc, char **argv){
-    FILE *file = fopen(argv[1], "r");
+	//This makes sure that the comman is the right length
+	if(argc != 3){
+		printf("Invalid use!");
+	}
 
-    unsigned char buffer[BUFFER_SIZE];
+	//Opens the file to read from
+	FILE *file = fopen(argv[1], "r");
 
-    size_t offset = 1157008;
+	//The save file into one buffer
+	unsigned char buffer[BUFFER_SIZE];
 
-    fseek(file, offset, SEEK_SET);
-    size_t size = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, file);
-    
-    unsigned int checksum = uShort(buffer, size);
+	//Safe way to get string->hex->decimal for the offset
+	size_t offset = strtoul(argv[2], (char **)0, 16);
 
-    //This is anding the value to only get the 4 byte answer we need
-    checksum = checksum & 0x00ffff;
+	//Offsets the file
+	fseek(file, offset, SEEK_SET);
+	//Reads the file into the buffer
+	size_t size = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, file);
 
-    printf("0x%X\n", checksum); //4dc5
+	//Final value of the checksum
+	unsigned int checksum = uShort(buffer, size);
 
-    return 0;
+	//This is anding the value to only get the 4 byte answer we need
+	checksum = checksum & 0x00ffff;
+
+	printf("0x%X\n", checksum); //4dc5
+
+	return 0;
 }
 
 unsigned int uShort(unsigned char *data, int bufSize){
-    int z;
-    unsigned int checksum = 0;
-    for(z = 0; z < bufSize - 1; z += 2){
-        checksum += ((ushort)data[z] << 8) + data[z + 1];
-    }
+	int z;
+	unsigned int checksum = 0;
+	//Calculates the checksum
+	for(z = 0; z < bufSize - 1; z += 2){
+		//Adds the bytes in together
+		checksum += ((ushort)data[z] << 8) + data[z + 1];
+	}
 
-    return checksum;
+	return checksum;
 }
